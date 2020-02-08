4be18aed64c7f59307886af7435466ca1e3e0c78
satyamsetia <satyam7991@gmail.com>
2020-02-08 20:27:18 +0530

    Feat: login form added

---
+++ src/components/LoginForm.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+
+export default class LoginForm extends Component {
+
+  state = {
+    username: '',
+    password: ''
+  }
+
+  handleUsernameChange(event) {
+    this.setState({
+      username: event.target.value,
+    })
+  }
+
+  handlePasswordChange(event) {
+    this.setState({
+      password: event.target.value,
+    })
+  }
+
+    handleSubmit(event) {
+      event.preventDefault();
+      console.log(this.state)
+    }
+
+  render() {
+    return (
+      <form onSubmit={(event) => this.handleSubmit(event)}>
+        <input type="text" placeholder="username" value={this.state.username} onChange={(event) => this.handleUsernameChange(event)}/>
+        <input type="password" placeholder="password" value={this.state.password} onChange={(event) => this.handlePasswordChange(event)}/>
+        <input type="submit" value="Login"/>
+      </form>
+    )
+  }
+}
